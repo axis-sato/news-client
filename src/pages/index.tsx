c8cf9bd4b0d2617bdf50b98ec480b49adf69b103
sato <sato_m@mediaxis.jp>
2020-05-16 23:05:29 +0900

    APIからaxiosとswrで記事一覧を取得する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,54 @@
 import { NextPage } from "next";
+import useSWR from "swr";
+import axios from "axios";
+
+interface Tag {
+  id: number;
+  name: string;
+}
+
+interface Site {
+  id: number;
+  name: string;
+}
+
+interface Article {
+  id: number;
+  title: string;
+  url: string;
+  image?: string;
+  crawledAt: string;
+  tags: Tag[];
+  sites: Site[];
+}
+
+const fetchArticles = async (): Promise<Article[]> => {
+  const resp = await axios.get("/api/v1/articles");
+  return resp.data.articles as Article[];
+};
 
 const Home: NextPage = () => {
-  return <>News</>;
+  const { data, error } = useSWR("articles", fetchArticles);
+  if (error) {
+    return <span>Error {error}</span>;
+  }
+  if (!data) {
+    return <span>Loading...</span>;
+  }
+
+  return (
+    <ul>
+      {data.map((a) => {
+        return (
+          <li key={a.id}>
+            <a target="_blank" href={a.url}>
+              {a.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Home;
